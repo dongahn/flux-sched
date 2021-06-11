c44a6ece1875bade5ecda50f0d0355be47327650
Dong H. Ahn <ahn1@llnl.gov>
2021-06-11 11:09:33 -0700

    util: add C++ support

--- src/common/libutil/xzmalloc.h
+++ src/common/libutil/xzmalloc.h
@@ -1,6 +1,10 @@
 #ifndef _UTIL_XZMALLOC_H
 #define _UTIL_XZMALLOC_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <sys/types.h>
 #include <stdarg.h>
 
@@ -8,6 +12,10 @@
  */
 void *xzmalloc (size_t size);
 char *xstrdup (const char *s);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* !_UTIL_XZMALLOC_H */
 /*
